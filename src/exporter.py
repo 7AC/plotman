95e4e07349835f22c60cf70efdcbd278d5efbea5
Giuseppe Valente <giusva@gmail.com>
2021-05-11 14:46:21 -0700

    exporter: add plots by phases

--- src/exporter.py
+++ src/exporter.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import time
-from plotman import configuration, job, reporting
+from plotman import configuration
 from plotman.job import Job
 from prometheus_client import start_http_server
 from prometheus_client.core import GaugeMetricFamily, REGISTRY
@@ -11,11 +11,16 @@ class PlotmanCollector:
 
     def collect(self):
         cfg = configuration.get_validated_configs()
-        jobs = Job.get_running_jobs(cfg.directories.log)
-
-        count = len(sorted(jobs, key=job.Job.get_time_wall))
-        yield GaugeMetricFamily("plotman_jobs_count", "Number of plotting jobs running", value=count)
-
+        running_jobs = Job.get_running_jobs(cfg.directories.log)
+        phases = {1: 0,
+                  2: 0,
+                  3: 0,
+                  4: 0}
+        for running_job in running_jobs:
+            phases[running_job.phase[0]] += 1
+        for phase in phases.keys():
+            yield GaugeMetricFamily(f"plotman_jobs_count_phase_{phase}",
+                                    f"Number of plotting jobs in phase {phase}", value=phases[phase])
 
 if __name__ == "__main__":
     start_http_server(8001)
