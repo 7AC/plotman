f71679f2cd640d86b9f478cfdd8f00786c29ce46
Giuseppe Valente <giusva@gmail.com>
2021-06-11 14:21:16 -0700

    exporter: adapt to new config

--- src/exporter.py
+++ src/exporter.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 
+import importlib
+import importlib.resources
 import time
 from plotman import configuration
+from plotman import resources as plotman_resources
 from plotman.job import Job
 from prometheus_client import start_http_server
 from prometheus_client.core import GaugeMetricFamily, REGISTRY
@@ -10,17 +13,25 @@ from prometheus_client.core import GaugeMetricFamily, REGISTRY
 class PlotmanCollector:
 
     def collect(self):
-        cfg = configuration.get_validated_configs()
-        running_jobs = Job.get_running_jobs(cfg.directories.log)
+        config_path = configuration.get_path()
+        config_text = configuration.read_configuration_text(config_path)
+        config_path = configuration.get_path()
+        config_text = configuration.read_configuration_text(config_path)
+        preset_target_definitions_text = importlib.resources.read_text(
+            plotman_resources, "target_definitions.yaml",
+        )
+        cfg = configuration.get_validated_configs(config_text, config_path, preset_target_definitions_text)
+        running_jobs = Job.get_running_jobs(cfg.logging.plots)
         phases = {1: 0,
                   2: 0,
                   3: 0,
                   4: 0}
         for running_job in running_jobs:
-            phases[running_job.phase[0]] += 1
+            phases[running_job.phase.major] += 1
         for phase in phases.keys():
             yield GaugeMetricFamily(f"plotman_jobs_count_phase_{phase}",
                                     f"Number of plotting jobs in phase {phase}", value=phases[phase])
+
 
 if __name__ == "__main__":
     start_http_server(8001)
